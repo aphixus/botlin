c0c0e376781fd024c278d42ffad0aa82b3ceb9f6
aphixus <nils.stenholm@gmail.com>
2015-10-31 17:41:06 +1100

    refactored helpers to apiserviceimpl

--- src/main/kotlin/fi/nsev/roadinfo/api/fluency/server/FreeFlowSpeedServiceImpl.kt
+++ src/main/kotlin/fi/nsev/roadinfo/api/fluency/server/FreeFlowSpeedServiceImpl.kt
@@ -2,6 +2,7 @@ package fi.nsev.roadinfo.api.fluency.server
 
 import fi.digitraffic.tie.sujuvuus.schemas.FreeFlowSpeedsResponse
 import fi.nsev.roadinfo.api.fluency.common.FreeFlowSpeedService
+import fi.nsev.roadinfo.api.server.ApiServiceImpl
 import org.springframework.stereotype.Service
 import java.io.ByteArrayInputStream
 import java.net.URL
@@ -13,25 +14,15 @@ import javax.xml.soap.SOAPMessage
 import javax.xml.ws.Service as wsService
 
 @Service
-class FreeFlowSpeedServiceImpl : FreeFlowSpeedService {
+class FreeFlowSpeedServiceImpl : FreeFlowSpeedService, ApiServiceImpl(){
 
     private final val FREE_FLOW_SPEED_URL : String = "http://tie.digitraffic.fi/sujuvuus/ws/freeFlowSpeeds"
     private final val FREE_FLOW_SPEED_WSDL_URL : URL = URL("http://tie.digitraffic.fi/sujuvuus/ws/freeFlowSpeeds?wsdl")
 
-    // https://stackoverflow.com/questions/13180372/creating-a-soapmessage-from-string-xml-of-entire-soap-message
-    private fun soapMessageFromString(xmlString : String) : SOAPMessage {
-        val factory : MessageFactory = MessageFactory.newInstance()
-        val inputStream : ByteArrayInputStream = ByteArrayInputStream(xmlString.toByteArray())
-        val message = factory.createMessage(MimeHeaders(), inputStream)
-        return message
-    }
-
-    //https://stackoverflow.com/questions/26097760/how-to-unmarshall-soap-xml-to-java-object
     override fun getFreeFlowSpeed() : FreeFlowSpeedsResponse {
-        val requestResult : String = URL(FREE_FLOW_SPEED_URL).readText()
-        val soapMsg : SOAPMessage = soapMessageFromString(requestResult)
         val unmarshaller : Unmarshaller = JAXBContext.newInstance(FreeFlowSpeedsResponse::class.java).createUnmarshaller()
-        val freeFlowSpeedRes : FreeFlowSpeedsResponse =  unmarshaller.unmarshal(soapMsg.soapBody.extractContentAsDocument()) as FreeFlowSpeedsResponse
+        val freeFlowSpeedRes : FreeFlowSpeedsResponse = queryApiUrl(FREE_FLOW_SPEED_URL, unmarshaller) as FreeFlowSpeedsResponse
+
         return freeFlowSpeedRes
     }
 
