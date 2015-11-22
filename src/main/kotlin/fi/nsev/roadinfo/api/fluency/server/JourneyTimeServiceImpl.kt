22255e36d63bda9f0c1a753774294cd61ea86772
nilss <nils.stenholm@gmail.com>
2015-11-22 14:53:13 +0200

    added service implementation

--- src/main/kotlin/fi/nsev/roadinfo/api/fluency/server/JourneyTimeServiceImpl.kt
+++ src/main/kotlin/fi/nsev/roadinfo/api/fluency/server/JourneyTimeServiceImpl.kt
@@ -4,11 +4,20 @@ import fi.digitraffic.tie.sujuvuus.schemas.JourneyTimeResponse
 import fi.nsev.roadinfo.api.fluency.common.JourneyTimeService
 import fi.nsev.roadinfo.api.server.ApiServiceImpl
 import org.springframework.stereotype.Service
+import java.net.URL
+import javax.xml.bind.JAXBContext
+import javax.xml.bind.Unmarshaller
 
 @Service
 class JourneyTimeServiceImpl : JourneyTimeService, ApiServiceImpl() {
 
+    private final val JOURNEY_TIME_URL : String = "http://tie.digitraffic.fi/sujuvuus/ws/journeyTime"
+    private final val JOURNEY_TIME_WSDL_URL : URL = URL("http://tie.digitraffic.fi/sujuvuus/ws/journeyTime?wsdl")
+
     override fun getJourneyTime(): JourneyTimeResponse {
-        throw UnsupportedOperationException()
+        val unmarshaller : Unmarshaller = JAXBContext.newInstance(JourneyTimeResponse::class.java).createUnmarshaller()
+        val journeyTimeRes : JourneyTimeResponse = queryApiUrl(JOURNEY_TIME_URL, unmarshaller) as JourneyTimeResponse
+
+        return journeyTimeRes
     }
 }
